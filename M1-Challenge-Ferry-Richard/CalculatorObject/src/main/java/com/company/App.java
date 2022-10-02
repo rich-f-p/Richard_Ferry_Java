00e0ddd8b2efe82b6803dc912ffde66604c8151d
rich-f-p <richardferry3@gmail.com>
2022-10-02 03:03:39 -0400

    add calculator class and print out simple calculations using int and double

---
+++ M1-Challenge-Ferry-Richard/CalculatorObject/src/main/java/com/company/App.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class App {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+
+        System.out.println("1+1= "+cal.add(1,1));
+        System.out.println("23-52= "+cal.subtract(23,52));
+        System.out.println("34*2= "+cal.multiply(34,2));
+        System.out.println("12/3= "+cal.divide(12,3));
+        System.out.println("12/7= "+cal.divide(12,7));
+        System.out.println("3.4+2.3= "+cal.add(3.4,2.3));
+        System.out.println("6.7*4.4= "+cal.multiply(6.7,4.4));
+        System.out.println("5.5-0.5= "+cal.subtract(5.5,0.5));
+        System.out.println("10.9/2.2= "+cal.divide(10.8,2.2));
+    }
+}
---
+++ M1-Challenge-Ferry-Richard/CalculatorObject/src/test/java/com/company/CalculatorTest.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    private Calculator cal;
+    @Before
+    public void setUp(){
+        cal = new Calculator();
+    }
+
+    @Test
+    public void shouldReturnTheSumOfTwoIntegers(){
+        
+    }
+    @Test
+    public void shouldReturnTheSumOfTwoDoubles(){
+
+    }
+    @Test
+    public void shouldReturnTheDifferenceOfTwoIntegers(){
+
+    }
+    @Test
+    public void shouldReturnTheDifferenceOfTwoDoubles(){
+
+    }
+    @Test
+    public void shouldReturnTheDivisionOfTwoIntegers(){
+
+    }
+    @Test
+    public void shouldReturnTheDivisionOfTwoDoubles(){
+
+    }
+    @Test
+    public void shouldReturnTheMultiplicationOfTwoIntegers(){
+
+    }
+    @Test
+    public void shouldReturnTheMultiplicationOfTwoDoubles(){
+
+    }
+}
