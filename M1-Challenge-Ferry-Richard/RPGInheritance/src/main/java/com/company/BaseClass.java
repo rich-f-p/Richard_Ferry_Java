000f33acd97842b447ee4ca64a93e9762f7ecde8
rich-f-p <richardferry3@gmail.com>
2022-10-04 04:07:12 -0400

    build: add parent class properties and abilities

--- M1-Challenge-Ferry-Richard/RPGInheritance/src/main/java/com/company/BaseClass.java
+++ M1-Challenge-Ferry-Richard/RPGInheritance/src/main/java/com/company/BaseClass.java
@@ -1,4 +1,103 @@
 package com.company;
 
 public class BaseClass {
+    private String name;
+    private int strength;
+    private int health;
+    private int stamina;
+    private int speed;
+    private int attackPower;
+    private boolean running;
+    private boolean arrested;
+
+    public void attackAnotherCharacter(BaseClass character){
+        System.out.println(name+": is attacking "+character.getName());
+        System.out.println(name+": Attack: "+attackPower);
+        stamina-=3;
+        character.setHealth(character.getHealth()-attackPower);
+        if(character.getHealth()<=0){
+            System.out.println(name+": defeated "+ character.getName());
+        }
+        System.out.println("Stamina remaining: "+stamina);
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public boolean isArrested() {
+        return arrested;
+    }
+
+    public void setArrested(boolean arrested) {
+        this.arrested = arrested;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseClass{" +
+                "name='" + name + '\'' +
+                ", strength=" + strength +
+                ", health=" + health +
+                ", stamina=" + stamina +
+                ", speed=" + speed +
+                ", attackPower=" + attackPower +
+                ", running=" + running +
+                ", arrested=" + arrested +
+                '}';
+    }
 }
