3e93b48447d2b86fe80e1924ee2227c9cca8edd5
rich-f-p <richardferry3@gmail.com>
2022-10-16 23:15:12 -0400

    add code for ErrorResponse object to the model folder

--- M2-Challenge-Ferry-Richard/src/main/java/com/company/M2ChallengeFerryRichard/model/ErrorResponse.java
+++ M2-Challenge-Ferry-Richard/src/main/java/com/company/M2ChallengeFerryRichard/model/ErrorResponse.java
@@ -1,4 +1,55 @@
 package com.company.M2ChallengeFerryRichard.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
 public class ErrorResponse {
+
+    private String errorMessage;
+    private int status;
+    private String errorCode;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    LocalDateTime time;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(String errorCode,String errorMessage) {
+        this.errorMessage = errorMessage;
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
 }
