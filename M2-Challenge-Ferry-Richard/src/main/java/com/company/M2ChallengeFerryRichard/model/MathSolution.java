fde6c90780aaff1d6ba7e3b4cb7b75c176333d13
rich-f-p <richardferry3@gmail.com>
2022-10-16 22:55:00 -0400

    add code for Month and MathSolution Objects to model folder

--- M2-Challenge-Ferry-Richard/src/main/java/com/company/M2ChallengeFerryRichard/model/MathSolution.java
+++ M2-Challenge-Ferry-Richard/src/main/java/com/company/M2ChallengeFerryRichard/model/MathSolution.java
@@ -1,5 +1,95 @@
 package com.company.M2ChallengeFerryRichard.model;
 
-public class MathSolution {
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
+public class MathSolution {
+    @NotNull(message = "operand one can not be null or empty")
+    private Integer operand1;
+    @NotNull(message = "operand two can not be null or empty")
+    private Integer operand2;
+    private String operation;
+    private Integer answer;
+
+    public MathSolution() {
+    }
+
+    public MathSolution(Integer operand1, Integer operand2, String operation) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.operation = operation;
+        this.answer = getAnswer(operation);
+    }
+
+    public int getAnswer(String operation){
+        int returnVal;
+        switch (operation){
+            case "add": returnVal = operand1+operand2;
+                break;
+            case "subtract": returnVal = operand1-operand2;
+                break;
+            case "multiply": returnVal = operand1*operand2;
+                break;
+            case "divide": returnVal = operand1/operand2;
+                break;
+            default: returnVal= Integer.parseInt(null);
+                break;
+        }
+        return returnVal;
+    }
+
+    public Integer getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(Integer operand1) {
+        this.operand1 = operand1;
+    }
+
+    public Integer getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(Integer operand2) {
+        this.operand2 = operand2;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public Integer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Integer answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "MathSolution{" +
+                "operand1=" + operand1 +
+                ", operand2=" + operand2 +
+                ", operation='" + operation + '\'' +
+                ", answer=" + answer +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MathSolution that = (MathSolution) o;
+        return Objects.equals(operand1, that.operand1) && Objects.equals(operand2, that.operand2) && Objects.equals(operation, that.operation) && Objects.equals(answer, that.answer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(operand1, operand2, operation, answer);
+    }
 }
--- M2-Challenge-Ferry-Richard/src/main/java/com/company/M2ChallengeFerryRichard/model/Month.java
+++ M2-Challenge-Ferry-Richard/src/main/java/com/company/M2ChallengeFerryRichard/model/Month.java
@@ -1,4 +1,91 @@
 package com.company.M2ChallengeFerryRichard.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
 public class Month {
+    @Min(value = 1, message = "input out of range")
+    @Max(value = 12,message = "input out of range")
+    private int number;
+    private String name;
+
+    public Month() {
+    }
+
+    public Month(int number) {
+        this.number = number;
+        this.name = convertMonth(number);
+    }
+
+    public String convertMonth(int num){
+        String returnVal = null;
+        switch (num){
+            case 1: returnVal = "January";
+                break;
+            case 2: returnVal = "February";
+                break;
+            case 3: returnVal = "March";
+                break;
+            case 4: returnVal = "April";
+                break;
+            case 5: returnVal = "May";
+                break;
+            case 6: returnVal = "June";
+                break;
+            case 7: returnVal = "July";
+                break;
+            case 8: returnVal = "August";
+                break;
+            case 9: returnVal = "September";
+                break;
+            case 10: returnVal = "October";
+                break;
+            case 11: returnVal = "November";
+                break;
+            case 12: returnVal = "December";
+                break;
+            default:
+                break;
+        }
+        return returnVal;
+    }
+
+    @Override
+    public String toString() {
+        return "Month{" +
+                "number=" + number +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Month month = (Month) o;
+        return number == month.number && Objects.equals(name, month.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, name);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
