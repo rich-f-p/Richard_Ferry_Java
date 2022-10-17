5b25598aff6b907876ec275c52baa2b27a4a7dcb
rich-f-p <richardferry3@gmail.com>
2022-10-16 23:18:38 -0400

    add routes for MonthMathController functionality, get month by number, a random month, add, subtract, multiply, and divide

--- M2-Challenge-Ferry-Richard/src/main/java/com/company/M2ChallengeFerryRichard/controllers/MonthMathController.java
+++ M2-Challenge-Ferry-Richard/src/main/java/com/company/M2ChallengeFerryRichard/controllers/MonthMathController.java
@@ -1,47 +1,62 @@
 package com.company.M2ChallengeFerryRichard.controllers;
 import com.company.M2ChallengeFerryRichard.model.MathSolution;
 import com.company.M2ChallengeFerryRichard.model.Month;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Random;
+
 @RestController
 public class MonthMathController {
-
+    private Month month;
+    private Random randomMonth = new Random();
 
     @RequestMapping(path = "/month/{monthNumber}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public Month monthConverter(@PathVariable int monthNumber){
-
+        if(monthNumber>12 || monthNumber<1)
+            throw new IllegalArgumentException("value is not within month range, range is 1-12");
+        return month = new Month(monthNumber);
     }
 
     @RequestMapping(path ="/randomMonth" ,method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public Month randomMonth(){
-
+        return month = new Month(randomMonth.nextInt(12)+1);
     }
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public MathSolution addInt(@RequestBody MathSolution math){
-
+    public MathSolution addInt(@RequestBody @Valid MathSolution math){
+            math.setOperation("add");
+            MathSolution newSol = new MathSolution(math.getOperand1(), math.getOperand2(), math.getOperation());
+            return newSol;
     }
 
     @RequestMapping(path = "/subtract",method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public MathSolution subtractInt(@RequestBody MathSolution math){
-
+    public MathSolution subtractInt(@RequestBody @Valid MathSolution math){
+        math.setOperation("subtract");
+        math.setAnswer(math.getOperand1()-math.getOperand2());
+        return math;
     }
 
     @RequestMapping(path = "/multiply", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public MathSolution multiplyInt(@RequestBody MathSolution math){
-
+    public MathSolution multiplyInt(@RequestBody @Valid MathSolution math){
+        math.setOperation("multiply");
+        math.setAnswer(math.getOperand1()* math.getOperand2());
+        return math;
     }
 
     @RequestMapping(path = "/divide", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public MathSolution divideInt(@RequestBody MathSolution math){
-
+    public MathSolution divideInt(@RequestBody @Valid MathSolution math){
+        math.setOperation("divide");
+        math.setAnswer(math.getOperand1()/math.getOperand2());
+        return math;
     }
 
 }
